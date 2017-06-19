486e435479ef42b28975415ca3ca3ca4441a7473
Oleksii Dovzhenko <zirkonus@gmail.com>
2017-06-19 15:12:39 +0300

    First commit

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # Small task accomplishment samples
 
-<b>simple1.php</b><br>
+<h3>sample1.php</h3>
 PHP Page required to create an HTML form that chooses a random line from a CSV file, and when it is filled, this line moves from this file to another CSV file.
 
-<b>simple2.php</b><br>
+<h3>sample2.php</h3>
 Work with one-click mobile payment API Fortumo.
 
-<b>simple3.php</b><br>
+<h3>sample3.php</h3>
 Function to calculate exponential moving average from array.
 
-<b>simple4.php</b><br>
+<h3>sample4.php</h3>
 Needs to work for BOTH querystring GET and JSON POST and writes the contents to a text file in JSON format.
 
-<b>simple5.php</b><br>
+<h3>sample5.php</h3>
 It's required to replace white and nearly white backgrounds with transparent ones and remove the empty space.
 
-<b>simple6.php</b><br>
+<h3>sample6.php</h3>
 Sudoku API library
+
+<h3>sample7.php</h3>
+The mayor of the town of Invoiceville has decided to publish the prices of every item on every menu of every restaurant in town, all in a single CSV file (Invoiceville is not quite up to date with modern data serialization methods). In addition, the restaurants of Invoiceville also offer Value Meals, which are groups of several items, at a discounted price. The mayor has also included these Value Meals in the file. More detailed description in file sample7.docx
+
+
---
+++ sample7.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * How to use
+ *
+ * 1. Place a file into some hosting.
+ * 2. Run somedomain.com/info.php in browser.
+ * 2.1. Select source file (must be txt or csv file).
+ * 2.2. Fill search field.
+ * 2.3. Click OK button.
+ * 2.4. Check result.
+ */
+
+if (is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+	if ( !($_FILES['userfile']['type'] == 'text/plain' || $_FILES['userfile']['type'] == 'text/csv') ) {
+		echo 'Wrong file! Please select another one.';
+	} elseif (empty(trim($_POST['search']))) {
+		echo 'Please fill all fields!';
+	} else {
+		$search = explode(', ', $_POST['search']);
+		// read source file
+		$file_handle = fopen($_FILES['userfile']['tmp_name'], "r");
+		if ($file_handle !== FALSE) {
+			// extract data
+			while (($line = fgets($file_handle)) !== FALSE) {
+				$tmp = explode(', ', trim($line));
+				$arr = array_slice($tmp, 2);
+				$items = array_combine($arr, array_fill(0, count($arr), $tmp[1]));
+				if ( isset($data[$tmp[0]]) ) {
+					$data[$tmp[0]] = array_merge($data[$tmp[0]], $items);
+				} else {
+					$data[$tmp[0]] = $items;
+				}
+			}
+			// calculation
+			foreach ($data as $k=>$v) {
+				$i = 0;
+				while ($search[$i]) {
+					if ( isset($v[trim($search[$i])]) ) {
+						$result[$k] += $v[trim($search[$i])]; 
+						$i++;
+					} else {
+						unset($result[$k]);
+						break;
+					}
+				}
+			}
+			// show result
+			if (!empty($result)) {
+				asort($result);
+				$id = key($result);
+				echo 'Restaurant: ' . $id . '<br />';
+				echo 'Total cost: ' . number_format($result[$id], 2);
+			} else {
+				echo 'Restaurant: none';
+			}
+		} else {
+			echo 'Something wrong with source file! Please select another one.';
+		}
+	}
+}
+?>
+
+<br /><br />
+<form method="POST" action"info.php" enctype="multipart/form-data">
+File: <input type="file" name="userfile" /><br />
+Search: <input type="text" name="search" value="<?=$_POST['search']?>" />
+<input type="submit" value="GO" />
+</form>
